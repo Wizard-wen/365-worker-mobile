40219ef5efff593ff397a7aaa5cd8f609c3a4ddf
wizard <sxw_1994@163.com>
2020-04-09 01:55:53 +0800

    加入我们页面开发

--- src/pages/join.vue
+++ src/pages/join.vue
@@ -12,7 +12,9 @@
             </div>
             
         </div>
+
         <van-form @submit="submit">
+            <h2 class="van-doc-demo-block__title">基本信息</h2>
             <van-field
                 v-model="joinForm.name"
                 name="姓名"
@@ -24,14 +26,11 @@
                 name="手机号"
                 label="手机号"
                 placeholder="请填写手机号"
-                :rules="[{ validator: checkWorkerPhone,required: true, message: '请填写手机号' }]"/>
-            <van-field
-                type="textarea"
-                name="个人信息"
-                v-model="joinForm.seller_remarks"
-                label="个人信息"
-                :rules="[{ required: true, message: '请填写您的个人情况' }]"
-                placeholder="请输入您的个人情况"/>
+                :rules="[
+                    { required: true,message: '只能是数字' },
+                    { validator: checkWorkerPhone,required: true, message: '请填写手机号' }
+                ]"/>
+            
             <van-field name="radio" label="性别">
                 <template slot="input">
                     <van-radio-group v-model="joinForm.sex" direction="horizontal">
@@ -45,15 +44,116 @@
                 name="身份证号"
                 label="身份证号"
                 placeholder="请填写身份证号"/>
-            <van-field name="age" label="生日">
+            
+            <van-field
+                readonly
+                clickable
+                name="datetimePicker"
+                v-model="joinForm.birthdayFormatter"
+                label="生日"
+                placeholder="点击选择生日"
+                @click="isBirthdayPopupShow = true"/>
+
+            <van-field
+                readonly
+                clickable
+                name="picker"
+                v-model="joinForm.educationFormatter"
+                label="学历"
+                placeholder="点击选择学历"
+                @click="isEducationPopupShow = true"/>
+            
+            
+            <van-field name="radio" label="婚姻状况">
                 <template slot="input">
-                    <van-datetime-picker
-                        v-model="joinForm.birthday"
-                        type="date"
-                        :min-date="minDate"
-                        :max-date="maxDate"/>
+                    <van-radio-group v-model="joinForm.is_married" direction="horizontal">
+                        <van-radio :name="1">是</van-radio>
+                        <van-radio :name="2">否</van-radio>
+                    </van-radio-group>
                 </template>
             </van-field> 
+
+            <van-field
+                readonly
+                clickable
+                name="picker"
+                v-model="joinForm.zodiac_signFormatter"
+                label="生肖"
+                placeholder="点击选择生肖"
+                @click="isZodiac_signPopupShow = true"/>
+            <van-field
+                v-model="joinForm.body_height"
+                name="身高"
+                label="身高"
+                placeholder="请填写身高"/>
+            
+            <van-field
+                v-model="joinForm.body_weight"
+                name="体重"
+                label="体重"
+                placeholder="请填写体重"/>
+            
+            <van-field name="uploader" label="头像">
+                <template slot="input">
+                    <van-uploader :after-read="uploadPicture" v-model="joinForm.icon" />
+                </template>
+            </van-field>
+            
+            <van-field
+                v-model="joinForm.address"
+                name="现住址"
+                label="现住址"
+                placeholder="请填写现住址"/>
+            <van-field
+                v-model="joinForm.urgent_phone"
+                name="紧急联系人"
+                label="紧急联系人"
+                placeholder="请填写紧急联系人"/>
+            <van-field
+                v-model="joinForm.address_in_law"
+                name="户籍地址"
+                label="户籍地址"
+                placeholder="请填写户籍地址"/>
+
+            <h2 class="van-doc-demo-block__title">备注信息</h2>
+            <van-field
+                type="textarea"
+                name="备注信息"
+                v-model="joinForm.remarks"
+                label="备注信息"
+                placeholder="请输入您的备注信息"/>
+            
+            <!-- 生日日期列表 -->
+            <van-popup v-model="isBirthdayPopupShow" position="bottom">
+                <van-datetime-picker
+                    type="date"
+                    v-model="currentDate"
+                    :min-date="minDate"
+                    :max-date="maxDate"
+                    @confirm="setBirthday"
+                    @cancel="isBirthdayPopupShow = false"/>
+            </van-popup>
+            
+            <!-- 学历列表 -->
+            <van-popup v-model="isEducationPopupShow" position="bottom">
+                <van-picker
+                    show-toolbar
+                    :value-key="'name'"
+                    :columns="workerConfigForm.educationList"
+                    @confirm="setEducation"
+                    @cancel="isEducationPopupShow = false"/>
+            </van-popup>
+
+            <!-- 生肖列表 -->
+            <van-popup v-model="isZodiac_signPopupShow" position="bottom">
+                <van-picker
+                    show-toolbar
+                    :value-key="'name'"
+                    :columns="workerConfigForm.zodiac_signList"
+                    @confirm="setZodiac_sign"
+                    @cancel="isZodiac_signPopupShow = false"/>
+            </van-popup>
+            
             <div style="margin: .5rem .1rem 0 .1rem;">
                 <van-button round block type="info" color="#ff7832" native-type="submit">
                     提交
@@ -64,7 +164,11 @@
 </template>
 
 <script>
+import {$utils} from '@/utils/index.js'
+import {educationList,zodiac_signList} from './join/Ijoin.js'
+
 import {joinService} from '@/service/join'
+
 import { Notify } from 'vant';
 import { Toast } from 'vant';
 // import { Image } from 'vant';
@@ -78,21 +182,89 @@ export default {
     data(){
         return {
             joinForm: {
-                name: '',
-                phone: '',
-                sex: 1,
-                identify: '',
-                birthday: new Date(),
+                name: '',//姓名
+                phone: '',//电话
+                sex: 1,//性别
+                identify: '',//身份证号
+                
+                birthday: 0,//生日
+                birthdayFormatter: '',
+                
+                education: 0,//教育程度
+                educationFormatter: '',
+               
+                is_married: 1,//婚姻状况
+
+                zodiac_sign: 0,//生肖
+                zodiac_signFormatter: '',
+                body_height: '',//身高
+                body_weight: '',
+                
+                icon:[],//头像
+
+                address: '',//现住址
+                urgent_phone: '',//紧急联系人
+                address_in_law: '',//户籍地址
+                
                 id:0,
-                seller_remarks: '',
+                remarks: '',
             },
-            minDate: new Date(2020, 0, 1),
+            
+            isBirthdayPopupShow: false,
+            isEducationPopupShow: false,
+            isZodiac_signPopupShow: false,
+            educationList: [{
+                id: 1,
+                name: '初中',
+            },{
+                id: 2,
+                name: '高中',
+            },],
+            workerConfigForm: {
+                educationList,
+                zodiac_signList,
+            },
+        
+            
+
+            currentDate: new Date(),
+            minDate: new Date(1900, 1, 1),
             maxDate: new Date(2025, 10, 1),
+            
             phoneError: '',
+            
             logo,
         }
     },
     methods: {
+        // 设置教育程度
+        setEducation(value) {
+            this.joinForm.education = value.id;
+            this.joinForm.educationFormatter = value.name;
+            this.isEducationPopupShow = false;
+        },
+        // 设置生日
+        setBirthday(value){
+            // 展示字段
+            this.joinForm.birthdayFormatter = $utils.formatDate(value, 'yyyy-MM-dd')
+            // 时间戳
+            this.joinForm.birthday = value.valueOf()
+            // 时间组件
+            this.isBirthdayPopupShow = false
+        },
+        // 设置生肖
+        setZodiac_sign(value){
+            // 展示字段
+            this.joinForm.zodiac_signFormatter =value.name;
+            // 
+            this.joinForm.zodiac_sign = value.id;
+            // 
+            this.isZodiac_signPopupShow = false
+        },
+        // 
+        uploadPicture(file){
+            console.log(file)
+        },
         async checkWorkerPhone(){
             let sendForm = {
                 phone: this.joinForm.phone,
@@ -170,5 +342,13 @@ export default {
     width: 80%;
     margin: 0 auto;
     display: block;
+}
+.van-doc-demo-block__title{
+    margin: 0;
+    padding: 32px 16px 16px;
+    color: rgba(69, 90, 100, 0.6);
+    font-weight: normal;
+    font-size: 14px;
+    line-height: 16px;
 }
 </style>
---
+++ src/pages/join/Ijoin.js
@@ -0,0 +1,25 @@
+// 教育程度
+export const educationList = [
+    {name: '全部',id: 0},
+    {name: '本科及以上',id: 1,},
+    {name: '专科',id: 2,},
+    {name: '高中',id: 3,},
+    {name: '初中',id: 4,},
+    {name: '小学',id: 5,},
+]
+// 生肖
+export const zodiac_signList =[
+    {name: '全部',id: 0},
+    {id: 1, name: '鼠'},
+    {id: 2, name: '牛'},
+    {id: 3, name: '虎'},
+    {id: 4, name: '兔'},
+    {id: 5, name: '龙'},
+    {id: 6, name: '蛇'},
+    {id: 7, name: '马'},
+    {id: 8, name: '羊'},
+    {id: 9, name: '猴'},
+    {id: 10, name: '鸡'},
+    {id: 11, name: '狗'},
+    {id: 12, name: '猪'},
+]
